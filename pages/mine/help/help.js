da8d2e55a37ed1abc7b91e78019f99f8efc4e2ed
unknown <unknown@example.com>
2019-01-22 20:11:19 +0800

    新增客服功能，恢复原来的搜索界面
    1.搜索功能待完善

--- pages/mine/help/help.js
+++ pages/mine/help/help.js
@@ -4,16 +4,7 @@ Page({
   data: {
     mail: "1175459652@qq.com"
   },
-  copymail(){
-    wx.setClipboardData({
-      data: this.data.mail,
-      success: function () {
-        wx.showToast({
-          title: '已复制到粘贴板',
-        })
-      }
-    })
-  }
+ 
 
 
 })
